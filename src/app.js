d4f1f45dc4df099c91bed00e8e134da7b8a8e01e
srushti <srushtipatel4040@gmail.com>
2024-08-07 10:25:52 +0530

    add user and video model

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ app.use(cors({
     credentials : true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
+app.use(express.json({limit: "16kb"})) // Middleware to parse incoming JSON requests with a size limit of 16 kilobytes. This is useful for handling JSON payloads in POST requests.
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // Middleware to parse incoming URL-encoded data (like form submissions). extended: true allows for rich objects and arrays to be encoded. The size limit is also 16 kilobytes.
+app.use(express.static("public"))  // Serves static files (like HTML, CSS, JavaScript) from the public directory. This means any files placed in the public folder can be accessed directly via their URL.
 app.use(cookieParser())
 
 export { app }
