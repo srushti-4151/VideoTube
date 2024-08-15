9b269399215a21502cdd387e5235e7f79ee8eeec
srushti <srushtipatel4040@gmail.com>
2024-08-15 10:29:34 +0530

    new updates

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = expres()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials : true
+    credentials : true //option allows cookies to be sent with cross-origin requests.
 }))
 
 app.use(express.json({limit: "16kb"})) // Middleware to parse incoming JSON requests with a size limit of 16 kilobytes. This is useful for handling JSON payloads in POST requests.
--- src/index.js
+++ src/index.js
@@ -24,6 +24,14 @@ connectDB().then(() => {
 // ;( async () => {
 //     try{
 //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERROR: ", error);
+//             throw error
+//         })
+    
+//         app.listen(process.env.PORT || 8000, () => {
+//             console.log(`sever is running at port : ${process.env.PORT}`);
+//         })  
 //     }catch(error){
 //         console.log("ERROR", error);
 //         throw error
